d04f08a5a07a8e5a2f0253a16d404a6eb899cfb6
den1slx <keycoingold@mail.ru>
2023-03-18 20:45:46 +1000

    Fixed who_entered

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,9 +1,8 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from datacenter.models import get_duration
 from datacenter.models import format_duration
+from datacenter.models import is_long_visit
 from django.shortcuts import render
-
 
 
 def storage_information_view(request):
@@ -12,9 +11,10 @@ def storage_information_view(request):
     for visit in visits:
         h, m = format_duration(get_duration(visit))
         visit_info = {}
-        visit_info['who_entered'] = visit.passcard
+        visit_info['who_entered'] = visit.passcard.owner_name
         visit_info['entered_at'] = visit.entered_at
         visit_info['duration'] = f'{h}:{m}'
+        visit_info['is_strange'] = is_long_visit(visit, h=1)
         non_closed_visits.append(visit_info)
 
     context = {
