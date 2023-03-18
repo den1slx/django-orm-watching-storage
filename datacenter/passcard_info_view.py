ef119b65b374d1bb8a42bc6211330a70f83d554a
den1slx <keycoingold@mail.ru>
2023-03-18 20:13:24 +1000

    Added passcard_info_view

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,35 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
+from datacenter.models import is_long_visit
+from datacenter.models import format_duration
+from datacenter.models import get_duration
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
+    this_passcard_visits = []
+    passcards = Passcard.objects.all()
+    passcard = get_object_or_404(passcards, passcode=passcode)
+    visits = Visit.objects.filter(passcard=passcard)
+    for visit in visits:
+        visit_info = {}
+        h, m = format_duration(get_duration(visit))
+        visit_info['entered_at'] = visit.entered_at
+        visit_info['duration'] = f'{h}:{m}'
+        visit_info['is_strange'] = is_long_visit(visit, h=1)
+        this_passcard_visits.append(visit_info)
+
     # Программируем здесь
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+
+    # this_passcard_visits = [
+    #     {
+    #         'entered_at': '11-04-2018',
+    #         'duration': '25:03',
+    #         'is_strange': False
+    #     },
+    # ]
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
